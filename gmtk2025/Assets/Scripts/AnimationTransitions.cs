414c72f95a5fe8d78ae74423d7a8341a0682b9b3
Douglas Choi <douglaschoi0910@gmail.com>
2025-08-02 19:56:17 +0100

    added bkg color changing when animation changes. fixed walk animation state machine

--- gmtk2025/Assets/Scripts/AnimationTransitions.cs
+++ gmtk2025/Assets/Scripts/AnimationTransitions.cs
@@ -1,17 +1,19 @@
-using Microsoft.Unity.VisualStudio.Editor;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AnimationTransitions : MonoBehaviour
 {
     Animator animator;
     int age;
     [SerializeField] int kidUpperBound, AdultUpperBound;
-    Image guyBkg;
+    [SerializeField] Color kidBkgColor, adultBkgColor, seniorBkgColor;
+    Graphic guyBkg;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         animator = GetComponent<Animator>();
+        guyBkg = GameObject.FindGameObjectWithTag("guyBkg").GetComponent<Graphic>();
     }
 
     // Update is called once per frame
@@ -23,18 +25,26 @@ public class AnimationTransitions : MonoBehaviour
         {
             //run kid animation logic
             animator.SetBool("isBaby", true);
+            SetBkgColour(kidBkgColor);
         }
         else if (age >= kidUpperBound && age < AdultUpperBound)
         {
             //run adult animation logic
             animator.SetBool("isBaby", false);
             animator.SetBool("isAdult", true);
+            SetBkgColour(adultBkgColor);
         }
         else
         {
             //run elder animation logic
             animator.SetBool("isAdult", false);
             animator.SetBool("isSenior", true);
+            SetBkgColour(seniorBkgColor);
         }
+    }
+
+    void SetBkgColour(Color c)
+    {
+        guyBkg.color = c;
     }
 }
